6f13dc42753d7bbd879035f2835875ea34bf53a1
Michael Corrieri <mlcorrieri@gmail.com>
2021-02-09 17:39:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,10 @@
 # code your #valid_move? method here
-def valid_move?(board, index)
-  if position_taken? && between?(0, 8)
+def valid_move?
+  if position_taken?
     return false
   else
-    return true
+    execute between?(0, 8)
+    
   end
 
 end
